fa29c5e9ad7a9be4233b1058d10c15f86164ea6f
Fieriko <111154099+Fieriko@users.noreply.github.com>
2023-11-23 10:07:34 +0000

    Scripts for Inventory and items(no UI)
    scripts for player inventory and an item data base nothing to display them yet tho

---
+++ Assets/Scripts/Inventory&Items/Inventory.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public ItemDataBase itemData;
+
+    public List<Item> PlayerItems = new List<Item> ();
+
+    public void GiveItem(int id)
+    {
+        Item item = itemData.GetItem(id);
+        PlayerItems.Add(item);
+    }
+    public Item CheckForItem(int id)
+    {
+        return PlayerItems.Find(item => item.Item_ID == id);
+    }
+    public void RemoveItem(int id)
+    {
+        Item item = CheckForItem(id);
+        if(item != null)
+        {
+            PlayerItems.Remove(item);
+        }
+    }
+}
