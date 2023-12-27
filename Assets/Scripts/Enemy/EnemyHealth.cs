2b93852288d571865717a1c7feff8ee5f2a1ffb9
Fieriko <111154099+Fieriko@users.noreply.github.com>
2023-12-27 17:14:37 +0000

    Enemy
    enemy is now stuned for a second when flashed. enemy can now patrol between two points

--- Assets/Scripts/Enemy/EnemyHealth.cs
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -6,17 +6,27 @@ public class EnemyHealth : MonoBehaviour
 {
     public int currentHealth;
     public int maxHealth;
+    private EnemyMovement enemMove;
 
     private void Awake()
     {
         currentHealth = maxHealth;
+        enemMove = GetComponent<EnemyMovement>();
     }
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
+        StartCoroutine(StopMove());
         if (currentHealth <= 0)
         {
             Destroy(gameObject);
         }        
+    }
+
+    IEnumerator StopMove()
+    {
+        enemMove.enabled = false;
+        yield return new WaitForSeconds(1);
+        enemMove.enabled = true;
     }
 }
--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -8,10 +8,44 @@ public class EnemyMovement : MonoBehaviour
     public float Speed;
     public int Damage;
     public float knockbackForce;
+    public bool patrol;
+
+    public Transform pointA;
+    public Transform pointB;
+
+    private bool movingTowardsA = true;
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, Target.position, Speed * Time.deltaTime);
+        if (patrol)
+        {
+            Patrol();
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, Target.position, Speed * Time.deltaTime);
+        }
+    }
+
+    void Patrol()
+    {
+        if (movingTowardsA)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, pointA.position, Speed * Time.deltaTime);
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, pointB.position, Speed * Time.deltaTime);
+        }
+
+        if (transform.position == pointA.position)
+        {
+            movingTowardsA = false;
+        }
+        else if (transform.position == pointB.position)
+        {
+            movingTowardsA = true;
+        }
     }
 }
